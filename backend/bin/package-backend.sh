2b04a844fbbb8719508d21a864a4c37dc93e311d
Victor <victor@andtaps.com>
2020-07-10 15:27:40 +0800

    fix: cicd settings

--- backend/bin/package-backend.sh
+++ backend/bin/package-backend.sh
@@ -6,10 +6,5 @@ set -e # fail script on any individual command failing
 
 DIR=`dirname "${BASH_SOURCE[0]}"`
 # build production static website
-cd ${DIR}/../generate
-npm install
-npm run test
-
-cd ${DIR}/../get
-npm install
-npm run test
+cd ${DIR}/..
+find ./* -maxdepth 1 -iname 'package.json' -not -path '*/node_modules/*' -execdir npm run package \;
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,5 @@
-export const API_ROOT = 'https://a.tinyurl.tech';
+// The deployment system does string substitution on variables within ${} prior to
+// uploading the website to S3.
+export const API_ROOT = '${BACKEND_ENDPOINT}'.includes('${')
+  ? 'https://a.tinyurl.tech'
+  : 'https://' + '${BACKEND_ENDPOINT}' + '/Prod';
