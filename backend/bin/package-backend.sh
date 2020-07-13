f62bd0cfa21739a3e3e8b247eee94cae3deb5872
Victor <victor@andtaps.com>
2020-07-13 08:59:57 +0800

    fix: cicd backend script return non-zero

--- backend/bin/package-backend.sh
+++ backend/bin/package-backend.sh
@@ -7,4 +7,8 @@ set -e # fail script on any individual command failing
 DIR=`dirname "${BASH_SOURCE[0]}"`
 # build production backend
 cd ${DIR}/..
-find ./* -maxdepth 1 -iname 'package.json' -not -path '*/node_modules/*' -execdir npm run package \;
+cd get
+npm run package 
+cd ..
+cd generate
+npm run package 
