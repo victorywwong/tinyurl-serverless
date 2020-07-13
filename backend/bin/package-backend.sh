4ecf52a8aa30ef2f124a7c6d69a47e38525c1825
Victor <victor@andtaps.com>
2020-07-13 08:39:12 +0800

    fix: backend package bash exit non 0

--- backend/bin/package-backend.sh
+++ backend/bin/package-backend.sh
@@ -7,4 +7,4 @@ set -e # fail script on any individual command failing
 DIR=`dirname "${BASH_SOURCE[0]}"`
 # build production backend
 cd ${DIR}/..
-find ./* -maxdepth 1 -iname 'package.json' -not -path '*/node_modules/*' -execdir npm run package \;
+find ./* -maxdepth 1 -iname 'package.json' -not -path '*/node_modules/*' -execdir npm run package \; ; 
